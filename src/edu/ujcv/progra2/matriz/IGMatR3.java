e2d00d673e78e2d609220da475c0fc314d112b4f
chriscastro85 <52246810+chriscastro85@users.noreply.github.com>
2019-07-10 01:49:13 -0500

    primera parte de las matrises completada y inicio del menu

--- src/edu/ujcv/progra2/matriz/IGMatR3.java
+++ src/edu/ujcv/progra2/matriz/IGMatR3.java
@@ -1,6 +1,28 @@
 package edu.ujcv.progra2.matriz;
 
+//import edu.ujcv.progra2.vector.IGVecR2;
+
+import edu.ujcv.progra2.vector.IGVecR2;
+import edu.ujcv.progra2.vector.IGVecR3;
+
 public class IGMatR3 {
+    private IGVecR3 colX;
+    private IGVecR3 colY;
+    private IGVecR3 colz;
+
+    public IGMatR3(){
+        colX = new IGVecR3(1,0,2);
+        colY = new IGVecR3(0,1,2);
+        colz = new IGVecR3(2,0,1);
+    }
+
+    public IGMatR3(IGVecR3 colX, IGVecR3 colY, IGVecR3 colz){
+        this.colX = new IGVecR2 (colX);
+        this.colY = new IGVecR2 (colY);
+
+
+    }
+
 
 
     // public static IGVecR2 rotacionZ (double angulo, IGVecR2 vector){
