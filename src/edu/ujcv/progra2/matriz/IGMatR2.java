272d07584d6289d7aee1a41e09ced6b5aeba2c64
chriscastro85 <52246810+chriscastro85@users.noreply.github.com>
2019-07-09 22:44:55 -0500

    primera parte de las matrises completada

--- src/edu/ujcv/progra2/matriz/IGMatR2.java
+++ src/edu/ujcv/progra2/matriz/IGMatR2.java
@@ -18,28 +18,79 @@ public class IGMatR2 {
     //IGVecR2 col2;
 
 
-
-//    public IGMatR2 mul(IGMatR2 b){
-//
-//    }
-//
+    private IGVecR2 colX;
+    private IGVecR2 colY;
 
 
-//    public IGVecR2 colXMat(IGVecR2 col){
-//
-//    }
+    public IGMatR2(){
+        colX = new IGVecR2(1,0);
+        colY = new IGVecR2(0,1);
+    }
 
-//    public IGMatR2 suma (IGMatR2 b){
-//
-//    }
+    public IGMatR2(IGVecR2 colX, IGVecR2 colY){
+        this.colX = new IGVecR2(colX);
+        this.colY = new IGVecR2(colY);
+    }
 
-//    public IGMatR2 escalarXmat(double escalar){
-//
-//    }
+    public IGMatR2(IGMatR2 b){
+        this.colX = new IGVecR2(b.colX);
+        this.colY = new IGVecR2(b.colY);
+    }
+    public IGVecR2 getColX() {
+        return colX;
+    }
 
-//    public static IGVecR2 rotacion (double angulo, IGVecR2 vector){
-//
-//    }
+    public void setColX(IGVecR2 colX) {
+        this.colX = colX;
+    }
+
+    public IGVecR2 getColY() {
+        return colY;
+    }
+
+    public void setColY(IGVecR2 colY) {
+        this.colY = colY;
+    }
+
+    public IGVecR2 getFilaX(){
+        return new IGVecR2(colX.getX(),colY.getX());
+    }
+    public IGVecR2 getFilaY(){
+        return new IGVecR2(colX.getY(),colY.getY());
+    }
+
+    public IGMatR2 mul(IGMatR2 b){
+        IGMatR2 retval = new IGMatR2();
+        retval.setColX(new IGVecR2(this.getFilaX().productoPunto(b.colX), this.getFilaY().productoPunto(b.colX)));
+        retval.setColY(new IGVecR2(this.getFilaX().productoPunto(b.colY),this.getFilaY().productoPunto(b.colY)));
+        return retval;
+    }
+
+
+    public IGVecR2 colXMat(IGVecR2 fila){
+        IGVecR2 retval = new IGVecR2();
+        retval.setX(fila.productoPunto(this.getFilaX()));
+        retval.setY(fila.productoPunto(this.getFilaY()));
+        return retval;
+    }
+
+    public IGMatR2 suma (IGMatR2 b){
+        IGMatR2 retval = new IGMatR2();
+        retval.setColX(this.colX.suma(b.colX));
+        retval.setColY(this.colY.suma(b.colY));
+        return retval;
+    }
+
+    public IGMatR2 escalarXmat(double escalar){
+        IGMatR2 retval = new IGMatR2();
+        retval.colX.setX(this.colX.getX() * escalar);
+        retval.colX.setY(this.colX.getY() * escalar);
+        retval.colY.setX(this.colY.getX() * escalar);
+        retval.colY.setY(this.colY.getY() * escalar);
+        return retval;
+    }
+
+
 
 
 }
--- src/edu/ujcv/progra2/vector/IGVecR4.java
+++ src/edu/ujcv/progra2/vector/IGVecR4.java
@@ -1,26 +1,60 @@
 package edu.ujcv.progra2.vector;
 
 public class IGVecR4 {
+    private double x;
+    private double y;
+    private double z;
+    private double w;
+
+    public IGVecR4(){
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.z = 0;
+    }
+    public IGVecR4(double x, double y, double z, double w){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+    public IGVecR4(IGVecR4 b){
+        this.x = b.x;
+        this.y = b.y;
+        this.z = b.z;
+        this.w = b.w;
+    }
     
     public IGVecR4 suma(IGVecR4 b){
-        //TODO: implementar
-        return new IGVecR4();
+        IGVecR4 a1 = new IGVecR4();
+        a1.x = x + b.x;
+        a1.y = y + b.y;
+        a1.z = z + b.z;
+        a1.w = w + b.w;
+        return a1;
+
     }
 
     public IGVecR4 resta(IGVecR4 b){
-        //TODO: implementar
-        return new IGVecR4();
+        IGVecR4 a1 = new IGVecR4();
+        a1.x = x - (b.x);
+        a1.y = y - (b.y);
+        a1.z = z - (b.z);
+        a1.w = w - (b.w);
+        return a1;
     }
 
-    //producto.
-    public  IGVecR4 escalarPorVector(double alpha){
-        //TODO: implementar
-        return new IGVecR4();
-    }
+
 
     public double productoPunto(IGVecR4 b){
-        //TODO: implementar
-        return 0.0;
+        IGVecR4 a1 = new IGVecR4();
+        a1.x = x * b.x;
+        a1.y = y * b.y;
+        a1.z = z * b.z;
+        a1.w = w * b.w;
+        double a = a1.x + a1.y + a1.z + a1.w;
+        return a;
+
     }
 
 
@@ -30,17 +64,31 @@ public class IGVecR4 {
     //}
 
 
-    public double determinante(){
-        //TODO: implementar
-        return 0.0;
-    }
+
 
     public double angulo(IGVecR4 b){
-        //TODO: implementar
-        return 0.0;
+        IGVecR4 a1 = new IGVecR4();
+        a1.x = x * b.x;
+        a1.y = y * b.y;
+        a1.z = z * b.z;
+        a1.w = w * b.w;
+        double a = a1.x + a1.y + a1.z + a1.w;
+        double c = Math.sqrt(((Math.pow(b.x,2)+Math.pow(b.y,2)+Math.pow(b.z,2)+Math.pow(b.w,2))));
+        double f = Math.sqrt(((Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2)+Math.pow(w,2))));
+        double d = Math.acos((a/(c*f)));
+
+        return d;
+
     }
 
-    public double magnitud(){
-        return Math.sqrt(productoPunto(this));
+    public double magnitud(IGVecR4 b){
+        IGVecR4 a1 = new IGVecR4();
+        a1.x = (b.x) - x;
+        a1.y = (b.y) - y;
+        a1.z = (b.z) - z;
+        a1.w = (b.w) - w;
+        double a = Math.pow(a1.x, 2) + Math.pow(a1.y,2) + Math.pow(a1.z,2) + Math.pow(a1.w,2);
+        return Math.sqrt(a);
     }
+
 }
